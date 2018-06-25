2c34fe14dea064e56f6e7a9bb8a86f3db2b653d6
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-06-25 16:43:02 +0400

    25/06/18

--- run_site_antoine.bat
+++ run_site_antoine.bat
@@ -2,4 +2,4 @@ mysql -u root site_project_database < site_project_database.sql
 
 xcopy ProgrammingAnts D:\wamp64\www\ProgrammingAnts /R /S /Y
 
-start http://localhost/ProgrammingAnts/html/home.html
+start http://localhost/ProgrammingAnts/html/home.php

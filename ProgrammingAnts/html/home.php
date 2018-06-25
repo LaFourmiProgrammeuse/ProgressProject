3403d4768146878e90f0b4e9e47c407661242229
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-06-25 18:06:46 +0400

    25/06/18

--- ProgrammingAnts/html/home.php
+++ ProgrammingAnts/html/home.php
@@ -1,17 +1,18 @@
-<<<<<<< HEAD:ProgrammingAnts/html/home.php
 <?php
 
 session_start();
 
 try{
 $bdd = new PDO('mysql:host=localhost;dbname=site_project_database;charset=utf8', 'root', '');
-$bdd.exec("INSERT INTO users (id, identifiant, mot_de_passe, mail) VALUES(\'\'zdzqdqdq\', \'antoine\', \'151172\', \'antoine.sauzeau@gmail.com\')");
+$req = $bdd->prepare('INSERT INTO Users (id, identifiant, mot_de_passe, mail) VALUES(:id, :identifiant, :mdp, :mail)');
+$req->execute(array('id' => '1', 'identifiant' => 'antoine', 'mdp' => '151172', 'mail' => 'antoinegggg'));
 }
 catch(Exception $e){
 	echo 'Erreur : ' . $e.getMessage();
 }
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -81,69 +82,3 @@ catch(Exception $e){
 		</body>
 
 </html>
-=======
-<!DOCTYPE html>
-<html>
-	
-	<head>
-
-		<link rel="stylesheet" type="text/css" href="../css/home_acceuil.css">
-		<meta charset="utf-8"></meta>
-		<title></title>
-
-			<style type="text/css">
-				a:link
-				{
-					text-decoration: none;
-				}
-			</style>
-
-	</head>
-
-		<body>
-			<header>
-				<span class="website_name"><h1>ProgrammingAnts</h1></span>
-				<img src="../images/programming_ants.png" class="programming_ants_logo" />
-
-					<div id="account">
-						
-						<div class="identification">Sign In</div>
-						<div class="identification">Sign Up</div>
-
-					</div>
-			</header>
-
-			<section>
-				<nav>
-				
-					<div id="top_menu">
-					
-						<div class="shortcut"><h3><a href="projects.html">Projects</a></h3></div>
-							<div class="shortcut"><h3><a href="forum.html">Forum</a></h3></div>
-								<div class="shortcut"><h3><a href="about.html">About</a></h3></div>
-									<div class="shortcut"><h3><a href="contact.html">Contact</a></h3></div>
-				
-					</div>			
-				</nav>
-
-				<article>
-					
-					<div id="article">
-						
-						
-
-					</div>
-
-				</article>
-			</section>
-
-			<footer>
-				
-				
-
-			</footer>
-
-		</body>
-
-</html>
->>>>>>> 11df6c06aaedd8b6f63ff113c7f774fd494b54e4:ProgrammingAnts/html/home.html~

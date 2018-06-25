b814946a012c4f3ad3b6339c07a97c92db3f0a1e
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-06-25 10:30:55 +0400

    Ajoout de init_bdd
    Permet d'actualiser la bdd

--- site_project_database.sql
+++ site_project_database.sql
@@ -14,6 +14,52 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `divers`
+--
+
+DROP TABLE IF EXISTS `divers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `divers` (
+  `visitor` int(11) DEFAULT NULL,
+  `visitor_now` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `divers`
+--
+
+LOCK TABLES `divers` WRITE;
+/*!40000 ALTER TABLE `divers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `divers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `identifiantt` varchar(22) NOT NULL,
+  `mot_de_passe` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,6 +70,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-/*Testsdsddsdqdsq*/;
-
--- Dump completed on 2018-06-23 11:48:55
+-- Dump completed on 2018-06-25  9:40:29

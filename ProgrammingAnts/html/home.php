c2dd467f78dd459ab06fb47999d5aaa69ff0cd22
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-06-30 18:15:29 +0400

    Ajout Disconnect.php

--- ProgrammingAnts/html/home.php
+++ ProgrammingAnts/html/home.php
@@ -35,7 +35,7 @@ echo 'connected = ' . $_SESSION['connected'] . '<br/>';
 							else{
                                 echo '<div id="account">';
                                     echo '<div class="identification">';
-                                        echo '<a href="Register_page.html">Se deconnecter</a>';
+                                        echo '<a href="Disconnect.php">Se deconnecter</a>';
                                     echo '</div>';
                                 echo '</div>';
 							}

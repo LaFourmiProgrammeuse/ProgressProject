773f0143d9292a6dd555e1229fa00834eb08bc41
Kreyse <floriantonty.tab@gmail;com>
2018-07-08 02:14:29 +0400

    08/07/2018

--- run_site_florian.bat
+++ run_site_florian.bat
@@ -2,4 +2,4 @@ mysql -u root site_project_database < site_project_database.sql
 
 xcopy ProgrammingAnts C:\wamp64\www\ProgrammingAnts /R /S /Y
 
-start http://localhost/ProgrammingAnts/html/home.php
+start http://localhost/ProgrammingAnts/html-php/home.php

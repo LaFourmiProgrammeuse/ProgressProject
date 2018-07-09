bcee88e12765a4a81ca92c2566045a8b0a6f7ebb
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-07-09 22:10:21 +0400

    Formulaire register javascript verif password

--- ProgrammingAnts/javascript/register.js
+++ ProgrammingAnts/javascript/register.js
@@ -1,12 +1,47 @@
-console.log("eedsq");
+var special_character = new Array('&', '@', '#');
+var special_character_needed = 2;
+
+var character_needed = 8;
+
+function hasSpecialCharacter(string){
+
+    var special_character_found = 0;
+
+    for(var i = 0; i < string.length; i++){
+
+        for(var e = 0; e < special_character.length; e++){
+
+            if(string[i] == special_character[e]){
+
+                special_character_found++;
+
+                if(special_character_found == special_character_needed){
+
+                    console.log("True");
+
+                     return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
 
 function PassValidation(){
-alert("Test");
 
-    console.log($("#pass").val());
+    var pass_value = $("#pass").val();
 
-    if($("#pass").val().length == 0){
-
+    if(pass_value.length == 0){
+        console.log("1");
+    }
+    else if(pass_value.length < character_needed || !hasSpecialCharacter(pass_value)){
+        console.log("2");
+        $("#validation_pass").src = "../images/wrong.png";
+    }
+    else{
+        console.log("3");
+        $("#validation_pass").src = "../images/check.png?"+new Date().getMilliseconds();
     }
 }
 

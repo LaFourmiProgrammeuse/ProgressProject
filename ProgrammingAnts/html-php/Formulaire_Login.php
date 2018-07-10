bb011b294ebdd2e8a3f1ba869932fbf132fc6c4f
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-07-10 18:31:39 +0400

    Ajout code foireux formulaire login

--- ProgrammingAnts/html-php/Formulaire_Login.php
+++ ProgrammingAnts/html-php/Formulaire_Login.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'Session_Control.php';
+
 try{
 
 $bdd = new PDO('mysql:host=localhost;dbname=site_project_database;charset=utf8', 'root', ''/*, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)*/);
@@ -12,5 +14,24 @@ catch(Exception $e){
 $nickname = $_POST["nickname"];
 $pass = $_POST["pass"];
 $stay_connected = $_POST["stay_connected"];
+
+if(isset($_COOKIE['identifiant']) AND isset($_COOKIE['mdp'])){
+    echo 'Tout les cookies existent';
+
+    setcookie('identifiant',  $nickname, time() + 365*24*3600, null, null, false, true);
+    setcookie('mdp',  $pass, time() + 365*24*3600, null, null, false, true);
+}
+else{
+    echo 'Un ou plusieurs cookie(s) n\'existent pas !';
+
+    setcookie('identifiant',  $nickname, time() + 365*24*3600);
+    setcookie('mdp',  $pass, time() + 365*24*3600);
+}
+
+$_SESSION['connected'] = 'true';
+$_SESSION['identifiant'] = $nickname;
+$_SESSION['mdp'] = $pass;
+
+header('Location: home.php');
 
 ?>

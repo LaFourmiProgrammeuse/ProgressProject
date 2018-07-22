18a8433b1cc3969e5dbda80fc2eec5d5d5f08daf
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-07-22 18:47:13 +0400

    22/27/18
    -Ajout + linkage des fichiers css des onglets du profil
    -Ajout 2 derniers onglets

--- ProgrammingAnts/javascript/profile.js
+++ ProgrammingAnts/javascript/profile.js
@@ -4,11 +4,19 @@ console.log("dsqd");
 
 $("#nav_element_overview").click(function(){
 console.log("zeqdsqdqd");
-    $("section").load("../html-php/onglets_profile/profile_overview.html");
+    $("#onglet_frame").load("../html-php/onglets_profile/profile_overview.html");
 });
 
 $("#nav_element_profile").click(function(){
-    $("section").load("../html-php/onglets_profile/profile_profile.html");
+    $("#onglet_frame").load("../html-php/onglets_profile/profile_profile.html");
+});
+
+$("#nav_element_account").click(function(){
+    $("#onglet_frame").load("../html-php/onglets_profile/profile_account.html");
+});
+
+$("#nav_element_other").click(function(){
+    $("#onglet_frame").load("../html-php/onglets_profile/profile_other.html");
 });
 
 });

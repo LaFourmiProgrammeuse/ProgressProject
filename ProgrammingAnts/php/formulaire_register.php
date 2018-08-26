ee67baca571c40952c1d19f6dd29f671019b12c1
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-08-26 09:21:02 +0400

    26/08/18
    -Modif info de connection bdd

--- ProgrammingAnts/php/formulaire_register.php
+++ ProgrammingAnts/php/formulaire_register.php
@@ -23,7 +23,7 @@ else{
 
 try{
 
-$bdd = new PDO('mysql:host=localhost;dbname=site_project_database;charset=utf8', 'root', ''/*, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)*/);
+$bdd = new PDO('mysql:host=programmpkroot.mysql.db;dbname=programmpkroot;charset=utf8', 'programmpkroot', 'BddProgAnts15');
 
 }
 catch(Exception $e){

2a662aae12b165baf7ef7a9cac94fefd48212a20
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-09-01 18:04:06 +0400

    01/08/18
    footer modified :
    - Links github, uses conditions, contact added

--- ProgrammingAnts/footer.php
+++ ProgrammingAnts/footer.php
@@ -1,5 +1,16 @@
+
 <footer>
-    <p id="footer_label">
-        © 2018 ProgrammingAnts, Inc.
-    </p>
+    <div id="footer_information_frame_1">
+        <p id="label_copyright">
+            © 2018 ProgrammingAnts, Inc.
+        </p>
+    </div>
+    <div id="footer_information_frame_2">
+        <h3 id="label_github"><a>Github</a></h3>
+        <p>-</p>
+        <h3 id="label_uses_condition"><a>Uses condition</a></h3>
+        <p>-</p>
+        <h3 id="label_contact"><a>Contact</a></h3>
+    </div>
+
 </footer>

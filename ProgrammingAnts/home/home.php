b17d00c830e39a69d03e2157b2886ad8d3dd2bca
Kreyse <floriantonty.tab@gmail;com>
2018-09-17 19:44:23 +0400

    17/09/2018
    +modifications home.php

--- ProgrammingAnts/home/home.php
+++ ProgrammingAnts/home/home.php
@@ -30,18 +30,18 @@ require '../php_for_all/session_control.php';
 			</div>
 
 			<div id="h_groupb">
-				<div class="nav_element"><h3><a href="#">ErrorWiki</a></h3></div>
-					<div class="nav_element"><h3><a href="../forum/forum.php">Forum</a></h3></div>
-						<div class="nav_element"><h3><a href="#">Projects</a></h3></div>
-							<div class="nav_element"><h3><a href="#">About</a></h3></div>
-								<div class="nav_element"><h3><a href="#">Contact</a></h3></div>
+				<div class="nav_element"><h3><a href="#"> <span class="object">></span> ErrorWiki</a></h3></div>
+					<div class="nav_element"><h3><a href="../forum/forum.php"> <span class="object">></span> Forum</a></h3></div>
+						<div class="nav_element"><h3><a href="#"> <span class="object">></span> Projects</a></h3></div>
+							<div class="nav_element"><h3><a href="#"> <span class="object">></span> About</a></h3></div>
+								<div class="nav_element"><h3><a href="#"> <span class="object">></span> Contact</a></h3></div>
 			</div>
 
 				<div id="h_groupc">
 					<div id="h_usera">
 						<div class="identification" id="login_button"><a href="../login/login_page.php">Log In</a></div>
 						<div class="identification" id="register_button"><a href="../register/register_page.php">Sign Up</a></div>
-						<div class="identification" id="disconnect_button"><a href="../php_for_all/disconnect.php">Sign Out</a></div>
+						<div class="identification" id="disconnect_button"><a href="../php_for_all/disconnect.php">Log Out</a></div>
 					</div>
 
 					<div id="h_userb">
@@ -56,32 +56,32 @@ require '../php_for_all/session_control.php';
 				<article>
 					<div id="articles">
 						<div class="article">
-							<h2>Projects</h2>
+							<h2>_ErrorWiki ></h2>
 							<p>No projects have been posted on our website for the moment, you should come back later, the ants will call you when the anthill is ready !</p>
 						</div>
 
 						<div class="article">
-							<h2>Forum</h2>
+							<h2>_Forum ></h2>
 							<p>Here is the forum, a place where you can talk with other people about a subject. It's the best place to go if you need help, the ants are amiable, they will help you with pleasure !
 							<p id="link_forum_article"><a href="../forum/forum.php" title="Forum" >Click here</a></p>
 						</div>
 
 						<div class="article">
-							<h2>Blog</h2>
+							<h2>_Projects ></h2>
 							<p>We still working on the blog for the moment, searching for ideas and training the ants !</p>
 						</div>
 
 						<div class="article">
-							<h2>About</h2>
-							<p><span class="bold">Programming Ants</span> is a project that we started to push our limits in programmation and to try to make something that can help people. For the moment, our website is <span class="bold">very limited in content</span>,
-								 we can't work faster because we still learning programmation languages (HTML5, PHP, CSS3, etc) to make our ideas done. The other cause is that we are only <span class="bold">two persons behind this project</span>, and it's difficult to work fastly,
-								 mostly when we have to solve <span class="bold">bugs and mistakes</span>. The fact that we are only teens and we have to study for school takes us a lot of time aswell. We won't give up and hope that everything will be fine in the futur !</p>
+							<h2>_About ></h2>
+							<p><b>Programming Ants</b> is a project that we started to push our limits in programmation and to try to make something that can help people. For the moment, our website is <b>very limited in content</b>,
+								 we can't work faster because we still learning programmation languages (HTML5, PHP, CSS3, etc) to make our ideas done. The other cause is that we are only <b>two persons behind this project</b>, and it's difficult to work fastly,
+								 mostly when we have to solve <b>bugs and mistakes</b>. The fact that we are only teens and we have to study for school takes us a lot of time aswell. We won't give up and hope that everything will be fine in the futur !</p>
 						</div>
 
 						<div class="article">
-							<h2>Contact</h2>
+							<h2>_Contact ></h2>
 							<p>If you want to contact us :</p>
-							<p><span class="bold">Programming Ants</span> e-mail adress : <a href="mailto:testp-a@gmail.com" title="Contact us !">testp-a@gmail.com</a></p>
+							<p><b>Programming Ants</b> e-mail adress : <a href="mailto:testp-a@gmail.com" title="Contact us !">testp-a@gmail.com</a></p>
 						</div>
 					</div>
 				</article>

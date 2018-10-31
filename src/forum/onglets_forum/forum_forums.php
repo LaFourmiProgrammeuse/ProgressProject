f16e7ce1eb7197bfa5a1bee1e0b90752902d5375
LaFourmieProgrammeuse <40513149+LaFourmiProgrammeuse@users.noreply.github.com>
2018-10-31 21:53:57 +0400

    Update src/forum/onglets_forum/forum_forums.php
    - Ajout de la syncro de la liste des forums avec la bdd

--- src/forum/onglets_forum/forum_forums.php
+++ src/forum/onglets_forum/forum_forums.php
@@ -1,3 +1,83 @@
+<?php
+
+    // On récupère le nom des forums par catégorie
+    $forums_language = array();
+    $forums_system = array();
+    $forums_software = array();
+    $forums_forum_functioning = array();
+
+    try{
+        $bdd = new PDO('mysql:host=programmpkroot.mysql.db;dbname=programmpkroot;charset=utf8', 'programmpkroot', 'BddProgAnts15');
+    }
+    catch(Exception $e){
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    $qprepare = $bdd->prepare("SELECT name, forum_type FROM forums");
+    $qprepare->execute();
+
+    while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
+
+        $qprepare_2 = $bdd->prepare("SELECT forum_desc, last_post_id, number_posts, number_topics FROM forums WHERE name=?");
+        $qprepare_2->execute(array($forum['name']));
+
+        $forum_information = $qprepare_2->fetch(PDO::FETCH_ASSOC);
+
+        if($forum_information['last_post_id'] != "-1"){
+
+            $qprepare_3 = $bdd->prepare("SELECT post_content, date_of_publication FROM posts WHERE id=?");
+            $qprepare_3->execute(array($forum_information['last_post_id']));
+
+            $last_post_information = $qprepare_3->fetch(PDO::ASSOC);
+
+            $forum_information['last_post_content'] = $last_post_information["post_content"];
+            $forum_information['date_of_publication'] = $last_post_information["date_of_publication"];
+        }
+        else{
+
+            $forum_information['last_post_content'] = "";
+            $forum_information['date_of_publication'] = "";
+        }
+
+        if($forum['forum_type'] == "language"){
+
+            $forums_language[$forum['name']] = ["forum_desc" => $forum_information['forum_desc'],
+            $forums_language[$forum['name']] = "post_content" => $forum_information['post_content'],
+            $forums_language[$forum['name']] = "date_of_publication" => $forum_information['date_of_publication'],
+            $forums_language[$forum['name']] = "number_posts" => $forum_information['number_posts'],
+            $forums_language[$forum['name']] = "number_topics" => $forum_information['number_topics']];
+        }
+        else if($forum["forum_type"] == "system"){
+
+            $forums_system[$forum['name']] = ["forum_desc" => $forum_information['forum_desc'],
+            $forums_system[$forum['name']] = "post_content" => $forum_information['post_content'],
+            $forums_system[$forum['name']] = "date_of_publication" => $forum_information['date_of_publication'],
+            $forums_system[$forum['name']] = "number_posts" => $forum_information['number_posts'],
+            $forums_system[$forum['name']] = "number_topics" => $forum_information['number_topics']];
+        }
+        else if($forum["forum_type"] == "software"){
+
+            $forums_software[$forum['name']] = ["forum_desc" => $forum_information['forum_desc'],
+            $forums_software[$forum['name']] = "post_content" => $forum_information['post_content'],
+            $forums_software[$forum['name']] = "date_of_publication" => $forum_information['date_of_publication'],
+            $forums_software[$forum['name']] = "number_posts" => $forum_information['number_posts'],
+            $forums_software[$forum['name']] = "number_topics" => $forum_information['number_topics']];
+        }
+        else if($forum["forum_type"] == "forum_functioning"){
+
+            $forums_forum_functioning[$forum['name']] = ["forum_desc" => $forum_information['forum_desc'],
+            $forums_forum_functioning[$forum['name']] = "post_content" => $forum_information['post_content'],
+            $forums_forum_functioning[$forum['name']] = "date_of_publication" => $forum_information['date_of_publication'],
+            $forums_forum_functioning[$forum['name']] = "number_posts" => $forum_information['number_posts'],
+            $forums_forum_functioning[$forum['name']] = "number_topics" => $forum_information['number_topics']];
+        }
+
+        //var_dump($forums_language);
+
+    }
+
+?>
+
 <head>
     <link rel="stylesheet" type="text/css" href="/src/forum/onglets_forum/css/forum_forums.css">
 </head>
@@ -17,39 +97,36 @@
                 <h3 class="h_forums_desc_3">Last message</h3>
             </div>
 
-            <div class="forum_language_desc">
-                <div class="forum_desc_groupa">
-                    <div class="forum_name">C++</div>
-                    <div class="forum_small_desc">A forum to answer your c++ questions</div>
-                </div>
-                <div class="forum_desc_groupb">
-                    <div class="n_messages_forum">40 messages </div>
-                    <div class="n_topics_forum">62 topics</div>
-                </div>
-                <div class="forum_desc_groupc">
-                    <div class="last_message_forum">
-                        <div class="message">Je suis un message</div>
-                        <div class="date_message">18/09/18</div>
+                <?php
+                    foreach($forums_language as $_forum_name => $_forum_information){
+                ?>
+                <div class="forum_language_desc">
+                    <div class="forum_desc_groupa">
+                        <div class="forum_name"><?php echo $_forum_name; ?></div>
+                        <div class="forum_small_desc"><?php echo $_forum_information['forum_desc']; ?></div>
                     </div>
-                </div>
-            </div>
+                    <div class="forum_desc_groupb">
+                        <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
+                        <div class="n_topics_forum"><?php echo ($_forum_information['number_posts'] . " topics"); ?></div>
+                    </div>
+                    <div class="forum_desc_groupc">
+                        <div class="last_message_forum">
 
-            <div class="forum_language_desc">
-                <div class="forum_desc_groupa">
-                    <div class="forum_name">Java</div>
-                    <div class="forum_small_desc">A forum to answer your java questions</div>
-                </div>
-                <div class="forum_desc_groupb">
-                    <div class="n_messages_forum">85 messages </div>
-                    <div class="n_topics_forum">70 topics</div>
-                </div>
-                <div class="forum_desc_groupc">
-                    <div class="last_message_forum">
-                        <div class="message">Je suis un message</div>
-                        <div class="date_message">23/02/18</div>
+                            <?php
+                                if($_forum_information['last_post_content'] == "" && $_forum_information['date_of_publication'] == ""){
+                            ?>
+                                <div class="message">No recently post</div>
+                                <div class="date_message"></div>
+                            <?php }else{ ?>
+                                <div class="message"><?php echo $_forum_information['last_post_content']; ?></div>
+                                <div class="date_message"><?php echo $_forum_information['date_of_publication']; ?></div>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                <?php } ?>
+
         </div>
 
 
@@ -60,78 +137,76 @@
                 <h3 class="h_forums_desc_3">Last message</h3>
             </div>
 
-            <div class="forum_language_desc">
-                <div class="forum_desc_groupa">
-                    <div class="forum_name">Window</div>
-                    <div class="forum_small_desc">A forum to answer your Window questions</div>
-                </div>
-                <div class="forum_desc_groupb">
-                    <div class="n_messages_forum">85 messages </div>
-                    <div class="n_topics_forum">70 topics</div>
-                </div>
-                <div class="forum_desc_groupc">
-                    <div class="last_message_forum">
-                        <div class="message">Je suis un message</div>
-                        <div class="date_message">23/02/18</div>
+                <?php
+                    foreach($forums_system as $_forum_name => $_forum_information){
+                ?>
+                <div class="forum_language_desc">
+                    <div class="forum_desc_groupa">
+                        <div class="forum_name"><?php echo $_forum_name; ?></div>
+                        <div class="forum_small_desc"><?php echo $_forum_information['forum_desc']; ?></div>
+                    </div>
+                    <div class="forum_desc_groupb">
+                        <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
+                        <div class="n_topics_forum"><?php echo ($_forum_information['number_posts'] . " topics"); ?></div>
+                    </div>
+                    <div class="forum_desc_groupc">
+                        <div class="last_message_forum">
+
+                            <?php
+                                if($_forum_information['last_post_content'] == "" && $_forum_information['date_of_publication'] == ""){
+                            ?>
+                                <div class="message">No recently post</div>
+                                <div class="date_message"></div>
+                            <?php }else{ ?>
+                                <div class="message"><?php echo $_forum_information['last_post_content']; ?></div>
+                                <div class="date_message"><?php echo $_forum_information['date_of_publication']; ?></div>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
+
+                <?php } ?>
+
             </div>
 
-            <div class="forum_language_desc">
-                <div class="forum_desc_groupa">
-                    <div class="forum_name">MacOs</div>
-                    <div class="forum_small_desc">A forum to answer your MacOS questions</div>
-                </div>
-                <div class="forum_desc_groupb">
-                    <div class="n_messages_forum">85 messages </div>
-                    <div class="n_topics_forum">70 topics</div>
-                </div>
-                <div class="forum_desc_groupc">
-                    <div class="last_message_forum">
-                        <div class="message">Je suis un message</div>
-                        <div class="date_message">23/02/18</div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="forum_language_desc">
-                <div class="forum_desc_groupa">
-                    <div class="forum_name">Linux</div>
-                    <div class="forum_small_desc">A forum to answer your linux questions</div>
-                </div>
-                <div class="forum_desc_groupb">
-                    <div class="n_messages_forum">85 messages </div>
-                    <div class="n_topics_forum">70 topics</div>
-                </div>
-                <div class="forum_desc_groupc">
-                    <div class="last_message_forum">
-                        <div class="message">Je suis un message</div>
-                        <div class="date_message">23/02/18</div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="article" id="forums_software">
+            <div class="article" id="forums_software">
             <div class="h_forums_desc">
                 <h3 class="h_forums_desc_1">Software</h3>
                 <h3 class="h_forums_desc_2">Topics/Messages</h3>
                 <h3 class="h_forums_desc_3">Last message</h3>
             </div>
 
-            <div class="forum_software_desc">
+            <?php
+                foreach($forums_software as $_forum_name => $_forum_information){
+            ?>
+            <div class="forum_language_desc">
                 <div class="forum_desc_groupa">
-                    <div class="forum_name"></div>
+                    <div class="forum_name"><?php echo $_forum_name; ?></div>
+                    <div class="forum_small_desc"><?php echo $_forum_information['forum_desc']; ?></div>
                 </div>
                 <div class="forum_desc_groupb">
-                    <div class="n_messages_forum"></div>
-                    <div class="n_topics_forum"></div>
+                    <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
+                    <div class="n_topics_forum"><?php echo ($_forum_information['number_posts'] . " topics"); ?></div>
                 </div>
                 <div class="forum_desc_groupc">
-                    <div class="last_message_forum"></div>
+                    <div class="last_message_forum">
+
+                        <?php
+                            if($_forum_information['last_post_content'] == "" && $_forum_information['date_of_publication'] == ""){
+                        ?>
+                            <div class="message">No recently post</div>
+                            <div class="date_message"></div>
+                        <?php }else{ ?>
+                            <div class="message"><?php echo $_forum_information['last_post_content']; ?></div>
+                            <div class="date_message"><?php echo $_forum_information['date_of_publication']; ?></div>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
+
+            <?php } ?>
+
         </div>
 
         <div class="article" id="forums_forum_functioning">
@@ -141,22 +216,35 @@
                 <h3 class="h_forums_desc_3">Last message</h3>
             </div>
 
-            <div class="forum_language_desc">
-                <div class="forum_desc_groupa">
-                    <div class="forum_name">Forum Rules</div>
-                    <div class="forum_small_desc">A forum speak about forum rules</div>
-                </div>
-                <div class="forum_desc_groupb">
-                    <div class="n_messages_forum">85 messages </div>
-                    <div class="n_topics_forum">70 topics</div>
-                </div>
-                <div class="forum_desc_groupc">
-                    <div class="last_message_forum">
-                        <div class="message">Je suis un message</div>
-                        <div class="date_message">23/02/18</div>
-                    </div>
-                </div>
-            </div>
+           <?php
+               foreach($forums_forum_functioning as $_forum_name => $_forum_information){
+           ?>
+           <div class="forum_language_desc">
+               <div class="forum_desc_groupa">
+                   <div class="forum_name"><?php echo $_forum_name; ?></div>
+                   <div class="forum_small_desc"><?php echo $_forum_information['forum_desc']; ?></div>
+               </div>
+               <div class="forum_desc_groupb">
+                   <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
+                   <div class="n_topics_forum"><?php echo ($_forum_information['number_posts'] . " topics"); ?></div>
+               </div>
+               <div class="forum_desc_groupc">
+                   <div class="last_message_forum">
+
+                       <?php
+                           if($_forum_information['last_post_content'] == "" && $_forum_information['date_of_publication'] == ""){
+                       ?>
+                           <div class="message">No recently post</div>
+                           <div class="date_message"></div>
+                       <?php }else{ ?>
+                           <div class="message"><?php echo $_forum_information['last_post_content']; ?></div>
+                           <div class="date_message"><?php echo $_forum_information['date_of_publication']; ?></div>
+                       <?php } ?>
+                   </div>
+               </div>
+           </div>
+
+           <?php } ?>
 
         </div>
     </article>

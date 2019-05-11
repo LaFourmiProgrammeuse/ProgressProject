938f46486cc99b275b3be4bda05e05be3f8fe70e
Kreyse <floriantonty.tab@gmail;com>
2019-05-12 01:48:49 +0400

    12/05/2019
    (+) nouvelle stylisation du forum en cours

--- src/forum/onglets_forum/forum_forums.php
+++ src/forum/onglets_forum/forum_forums.php
@@ -123,14 +123,6 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
         $forums_software[$forum['name']] = "number_posts" => $forum_information['number_posts'],
         $forums_software[$forum['name']] = "number_topics" => $forum_information['number_topics']];
     }
-    else if($forum["forum_type"] == "forum_functioning"){
-
-        $forums_forum_functioning[$forum['name']] = ["forum_desc" => $forum_information['forum_desc'],
-        $forums_forum_functioning[$forum['name']] = "last_active_topic_title" => $forum_information['last_active_topic_title'],
-        $forums_forum_functioning[$forum['name']] = "lat_time_from_last_activity" => $forum_information['lat_time_from_last_activity'],
-        $forums_forum_functioning[$forum['name']] = "number_posts" => $forum_information['number_posts'],
-        $forums_forum_functioning[$forum['name']] = "number_topics" => $forum_information['number_topics']];
-    }
 
         //var_dump($forums_language);
 
@@ -151,9 +143,9 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
     <div id="top_nav">
       <div id="tn_groupa">
 
-        <a href="#" class="text_element">Choose a forum</a>
-        <a href="forum_stats.php" class="text_element">Stats</a>
-        <a href="forum_rules.php" class="text_element">Rules</a>
+        <div class="tn_groupa_elem"><a href="#">Choose a forum</a></div>
+        <div class="tn_groupa_elem"><a href="forum_stats.php" class="tn_groupa_elem">Stats</a></div>
+        <div class="tn_groupa_elem"><a href="forum_rules.php" class="tn_groupa_elem">Rules</a></div>
 
       </div>
 
@@ -161,7 +153,7 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
 
         <div class="searchbar">
           <form action="/search" id="searchthis" method="get">
-            <input id="search" name="q" type="text" placeholder="Type here to search" />
+            <input id="search" name="tn_searchbar" type="text" placeholder="Type here to search" />
             <input id="search-btn" type="submit" value="Ok" />
           </form>
         </div>
@@ -175,6 +167,7 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
         <div class="f_category" id="f_languages">
           <h1 class="category_title">Languages</h1>
 
+          <div class="f_list">
           <?php
               foreach($forums_language as $_forum_name => $_forum_information){
           ?>
@@ -183,18 +176,18 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
                   <div class="forum_name"><?php echo $_forum_name; ?></div>
                   <div class="forum_small_desc"><?php echo $_forum_information['forum_desc']; ?></div>
               </div>
+
               <div class="forum_desc_groupb">
-                  <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
                   <div class="n_topics_forum"><?php echo ($_forum_information['number_topics'] . " topics"); ?></div>
-              </div>
-              <div class="forum_desc_groupc">
                   <div class="last_message_forum">
+                      <div class="lmf_inf">Last message :</div>
                       <div class="message"><?php echo $_forum_information['last_active_topic_title']; ?></div>
-                      <div class="date_message"><?php echo $_forum_information['lat_time_from_last_activity']; ?></div>
                   </div>
+                  <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
               </div>
           </div>
           <?php } ?>
+          </div>
 
         </div>
 
@@ -203,6 +196,7 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
         <div class="f_category" id="f_systems">
           <h1 class="category_title">Operating systems</h1>
 
+          <div class="f_list">
           <?php
               foreach($forums_system as $_forum_name => $_forum_information){
           ?>
@@ -211,18 +205,18 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
                   <div class="forum_name"><?php echo $_forum_name; ?></div>
                   <div class="forum_small_desc"><?php echo $_forum_information['forum_desc']; ?></div>
               </div>
+
               <div class="forum_desc_groupb">
-                  <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
                   <div class="n_topics_forum"><?php echo ($_forum_information['number_topics'] . " topics"); ?></div>
-              </div>
-              <div class="forum_desc_groupc">
                   <div class="last_message_forum">
+                      <div class="lmf_inf">Last message :</div>
                       <div class="message"><?php echo $_forum_information['last_active_topic_title']; ?></div>
-                      <div class="date_post"><?php echo $_forum_information['lat_time_from_last_activity']; ?></div>
                   </div>
+                  <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
               </div>
           </div>
           <?php } ?>
+          </div>
 
         </div>
 
@@ -231,6 +225,7 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
         <div class="f_category" id="f_softwares">
           <h1 class="category_title">Softwares</h1>
 
+          <div class="f_list">
           <?php
               foreach($forums_software as $_forum_name => $_forum_information){
           ?>
@@ -239,18 +234,18 @@ while($forum = $qprepare->fetch(PDO::FETCH_ASSOC)){
                   <div class="forum_name"><?php echo $_forum_name; ?></div>
                   <div class="forum_small_desc"><?php echo $_forum_information['forum_desc']; ?></div>
               </div>
+
               <div class="forum_desc_groupb">
-                  <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
                   <div class="n_topics_forum"><?php echo ($_forum_information['number_topics'] . " topics"); ?></div>
-              </div>
-              <div class="forum_desc_groupc">
                   <div class="last_message_forum">
+                      <div class="lmf_inf">Last message :</div>
                       <div class="message"><?php echo $_forum_information['last_active_topic_title']; ?></div>
-                      <div class="date_message"><?php echo $_forum_information['lat_time_from_last_activity']; ?></div>
                   </div>
+                  <div class="n_messages_forum"><?php echo ($_forum_information['number_posts'] . " posts"); ?></div>
               </div>
           </div>
           <?php } ?>
+          </div>
 
         </div>
 

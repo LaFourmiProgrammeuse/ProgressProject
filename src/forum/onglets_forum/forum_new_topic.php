fefbc1dc8dbbebe123d10d1c4c1a70f909ea9669
Kreyse <floriantonty.tab@gmail;com>
2019-07-26 00:23:04 +0400

    25/07/2019
    - stylisation de l'onglet "new_topic" presque terminée
    - modification de certains noms de balises dans le HTML de l'onglet "new_topic"

--- src/forum/onglets_forum/forum_new_topic.php
+++ src/forum/onglets_forum/forum_new_topic.php
@@ -5,32 +5,32 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="/src/forum/onglets_forum/css/forum_new_topic.css" />
 
-    <link href="https://fonts.googleapis.com/css?family=Mandali&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Mandali&display=swap" rel="stylesheet">
 </head>
 
 <div class="central_onglet_body">
 
 <article>
-  <div class="form_hr">
+  <div id="form_header">
     <h1>Create a new topic</h1>
   </div>
 
-	<div class="form_sect">
+	<div id="form_sect">
 		<form id="form_new_topic" method="post" action="/src/forum/php/create_topic.php">
-			<h3>Title</h3>
-				<input type="text" name="topic_title" class="topic_title" />
+			<h3 class="label_input">Title</h3>
+				<input type="text" name="topic_title" id="topic_title" />
 
-      <h3 class="label_message">Your message</h3>
-				<div class="msge_hr">
+      <h3 class="label_input">Your message</h3>
+				<div id="msge_header">
 					<div class="text_formatting">
 						<div class="text_style">
 							<span class="label">Style</span>
-							<img src="/images/arrows/a_down.png" />
+							<img class="a_down" src="/images/arrows/a_down.png" />
 						</div>
 
 						<div class="text_size">
 							<span class="label">Size</span>
-							<img src="/images/arrows/a_down.png" />
+							<img class="a_down" src="/images/arrows/a_down.png" />
 						</div>
 
 						<div class="text_bold">
@@ -59,11 +59,11 @@
 					</div>
 				</div>
 
-			<div class="msge_ctent">
+			<div class="msge_content">
 				<textarea form="form_new_topic" name="first_post_content"></textarea>
 			</div>
 
-			<div class="message_footer">
+			<div class="msge_footer">
 				<div class="send">
 					<input type="button" name="send" value="Send" class="send" />
 				</div>
